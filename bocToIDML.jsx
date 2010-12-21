f3fe83a7736cf442c04ebfff61f89040255e507a
fabiantheblind <fabiantheblind@googlemail.com>
2010-12-21 09:36:41 +0100

    dont need these anymore

--- bocToIDML.jsx
+++
@@ -1,1 +0,0 @@
-﻿var theFolder= "~/Desktop/idml";var doc = app.activeDocument;var aName = doc.name;var newName = aName.replace("indd","idml");var theFile = File(theFolder +"/"+ newName );doc.exportFile(ExportFormat.INDESIGN_MARKUP, theFile, false);
